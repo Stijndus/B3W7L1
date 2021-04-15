3e451b3fd784f7f2f51f09bea31c8c3db655b4f7
Stijn <stijn@ardusit.nl>
2021-04-15 10:49:03 +0200

    B3W7L3

--- index.php
+++ index.php
@@ -13,15 +13,20 @@
 <head>
     <meta charset="UTF-8">
     <title>All Characters</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link href="resources/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
     <header>
         <h1>Alle <?php echo $count; ?> characters uit de database</h1>
-
     </header>
+    <nav class="navbar">
+        <ul class="navlist">
+            <li class="list-item"><a href="locations.php"><i class="fas fa-compass"></i>Locations</a></li>
+        </ul>
+    </nav>
     <div id="container">
         <?php   foreach($result as $row){ ?>
         <a class="item" href="character.php?charId=<?php echo $row['id'] ?>">
@@ -34,8 +39,10 @@
                     <ul class="fa-ul">
                         <li><span class="fa-li"><i class="fas fa-home"></i></span> <?php echo $row['location'] ?></li>
                         <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $row['health'] ?></li>
-                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $row['attack'] ?></li>
-                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $row['defense'] ?></li>
+                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $row['attack'] ?>
+                        </li>
+                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $row['defense'] ?>
+                        </li>
                     </ul>
                 </div>
             </div>
---
+++ locations.php
@@ -0,0 +1,34 @@
+<?php
+
+    include 'functions/db_con.php';
+    include 'functions/functions.php';
+
+    $result = selectAllLocations();
+
+    $count  = count($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=	, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="resources/css/style.css" rel="stylesheet" />
+    <title>Locations</title>
+</head>
+<body>
+<header>
+        <h1>Alle <?php echo $count; ?> locaties uit de database</h1>
+        <a class="backbutton" href="index.php"><i class="fas fa-long-arrow-alt-left"></i> Terug</a>
+    </header>
+    <div id="container">
+        <h2>Name</h2>
+        <?php foreach($result as $row){ ?>
+            <p class="locationName"><?php echo $row['name']; ?></p>
+        <?php } ?>
+    </div>
+    <footer>&copy; Stijn Dusseldorp 2020</footer>
+</body>
+</html>
